dcf79fb4eba68406cce9dd25da06ce9d81dcfcd1
vishalraj258 <vishalraj258@gmail.com>
2019-08-19 10:54:24 +0530

    Updates

---
+++ precache-manifest.39e369a7c62b024c4f6bb027036c9376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f09eb74720150fcc43f93d88900d53",
+    "url": "/mini-phonebook/index.html"
+  },
+  {
+    "revision": "d15f1ad3515fcf6996d9",
+    "url": "/mini-phonebook/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ad07cc3f1356c9d26e15",
+    "url": "/mini-phonebook/static/css/main.80f5da66.chunk.css"
+  },
+  {
+    "revision": "d15f1ad3515fcf6996d9",
+    "url": "/mini-phonebook/static/js/2.b5485dc2.chunk.js"
+  },
+  {
+    "revision": "ad07cc3f1356c9d26e15",
+    "url": "/mini-phonebook/static/js/main.f3db0f0c.chunk.js"
+  },
+  {
+    "revision": "093320e506b248b1d98a",
+    "url": "/mini-phonebook/static/js/runtime~main.0f039433.js"
+  }
+]);
